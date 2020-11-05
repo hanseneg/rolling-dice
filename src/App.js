5f8576ef829b0f51b1fcf5e34924709035d6a902
Ethan Hansen <hanseneg@hotmail.com>
2020-11-05 14:50:04 -0700

    commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Dicebox from './Dicebox'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            num1: 0,
+            num2: 0,
+            num3: 0,
+            num4: 0,
+            num5: 0
+        }
+    }
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
